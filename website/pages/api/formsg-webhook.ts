8604961bbec410054b3dc1a102fb63fffbef5cd8
thorwebdev <thor@supabase.io>
2022-01-17 17:04:49 +0800

    Add FormSG webhook scaffolding.

---
+++ website/pages/api/formsg-webhook.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from "next";
+const formsg = require("@opengovsg/formsg-sdk")({
+  mode: "production",
+});
+
+const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    try {
+      // Endpoint authentication by verifying signatures
+      formsg.webhooks.authenticate(
+        req.headers["X-FormSG-Signature"],
+        process.env.FORMSG_WEBHOOK_POST_URI
+      );
+      // Continue processing the POST body
+      // If `verifiedContent` is provided in `req.body.data`, the return object
+      // will include a verified key.
+      const submission = process.env.FORMSG_HAS_ATTACHMENTS
+        ? await formsg.crypto.decryptWithAttachments(
+            process.env.FORMSG_SECRET_KEY,
+            req.body.data
+          )
+        : formsg.crypto.decrypt(process.env.FORMSG_SECRET_KEY, req.body.data);
+
+      // If the decryption failed, submission will be `null`.
+      if (submission) {
+        // Continue processing the submission
+        console.log(JSON.stringify(submission, null, 2));
+      } else {
+        // Could not decrypt the submission
+        console.log("Could not decrypt the submission");
+      }
+    } catch (e) {
+      return res.status(401).end("Unauthorized");
+    }
+
+    res.json({ received: true });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+};
+
+export default webhookHandler;
