86248542eceeb1abf05b8c4a08778ff819a6cff9
Antariksh <antarikshmahajan@gmail.com>
2022-01-25 23:28:51 +0800

    style: create Header

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,35 +1,42 @@
-import { SimpleGrid, Image, useColorMode, Flex } from "@chakra-ui/react";
+import { SimpleGrid, Image, useColorMode, Flex, VStack } from '@chakra-ui/react'
+import { Header } from '../components/Header'
 
-import { Screen } from "../components/Screen";
-import { Tile } from "../components/Tile";
+import { Screen } from '../components/Screen'
+import { Tile } from '../components/Tile'
 
 const Index = () => {
-  const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode()
 
   return (
-    <Screen minH="100vh" p={5}>
+    <VStack>
+      <Header />
+    </VStack>
+  )
+
+  return (
+    <Screen minH='100vh' p={5}>
       <Image
-        htmlWidth="20%"
-        position="fixed"
-        top="0"
-        src="UnboringSG.png"
-        alt="UnboringSG logo"
+        htmlWidth='20%'
+        position='fixed'
+        top='0'
+        src='UnboringSG.png'
+        alt='UnboringSG logo'
       />
-      <SimpleGrid maxW="100%" minChildWidth="350px" spacing={5}>
-        <Tile title={"EAT"} />
-        <Tile title={"DO"} />
-        <Tile title={"LEARN"} />
+      <SimpleGrid maxW='100%' minChildWidth='350px' spacing={5}>
+        <Tile title={'EAT'} />
+        <Tile title={'DO'} />
+        <Tile title={'LEARN'} />
       </SimpleGrid>
-      <Flex position="fixed" bottom="1rem" right="1rem">
+      <Flex position='fixed' bottom='1rem' right='1rem'>
         <Image
-          htmlWidth="150px"
+          htmlWidth='150px'
           src={`ogp_logo_${colorMode}.svg`}
-          alt="OGP Logo"
+          alt='OGP Logo'
         />
-        <Image htmlWidth="90px" src="stb_logo.svg" alt="STB Logo" pl={2} />
+        <Image htmlWidth='90px' src='stb_logo.svg' alt='STB Logo' pl={2} />
       </Flex>
     </Screen>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
