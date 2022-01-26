7e0403f0d069693b7344be5b4d1283eb53fddf6b
thorwebdev <thor@supabase.io>
2022-01-26 14:19:04 +0800

    chore: text flex end

--- website/components/Tile/Rec.tsx
+++ website/components/Tile/Rec.tsx
@@ -1,40 +1,43 @@
 import {
   Box,
   Center,
+  Flex,
   Skeleton,
   Text,
   useBreakpointValue,
   useColorMode,
-} from '@chakra-ui/react'
-import { definitions } from '../../types/supabase'
-import { LoadedRec } from './LoadedRec'
+} from "@chakra-ui/react";
+import { definitions } from "../../types/supabase";
+import { LoadedRec } from "./LoadedRec";
+import { useRecs } from "./useRecs";
 
 interface RecProps {
-  isLoading: boolean
-  rec: definitions['recommendations'] | null
+  isLoading: boolean;
+  rec: definitions["recommendations"] | null;
   handleClick: (
-    event?: 'increment_clicks' | 'increment_downvotes',
-  ) => Promise<void>
+    event?: "increment_clicks" | "increment_downvotes"
+  ) => Promise<void>;
 }
 
-export const Rec = ({ isLoading, rec, handleClick }: RecProps) => {
-  const { colorMode } = useColorMode()
-  const isMobile = useBreakpointValue({ base: true, xs: true, md: false })
+export const Rec = (props) => {
+  const { currentRec, handleClick, isLoading } = useRecs(props.title);
+  const { colorMode } = useColorMode();
+  const isMobile = useBreakpointValue({ base: true, xs: true, md: false });
 
   if (isLoading) {
-    return <Skeleton h='100%' w='100%' />
+    return <Skeleton h="100%" w="100%" />;
   }
   return (
-    <Box h='100%'>
-      {rec ? (
-        <LoadedRec rec={rec} handleClick={handleClick} />
+    <Flex {...props}>
+      {currentRec ? (
+        <LoadedRec rec={currentRec} handleClick={handleClick} />
       ) : (
-        <Center h='100%' mx={isMobile ? '2rem' : '4rem'} textAlign={'center'}>
-          <Text textStyle='body-1' color='neutral.700'>
+        <Center h="100%" mx={isMobile ? "2rem" : "4rem"} textAlign={"center"}>
+          <Text textStyle="body-1" color="neutral.700">
             That's all for now, check back later!
           </Text>
         </Center>
       )}
-    </Box>
-  )
-}
+    </Flex>
+  );
+};
--- website/components/Tile/RecText.tsx
+++ website/components/Tile/RecText.tsx
@@ -1,22 +1,32 @@
-import { Flex, Text } from '@chakra-ui/react'
-import { definitions } from '../../types/supabase'
+import { Flex, Text, useColorMode } from "@chakra-ui/react";
+import { definitions } from "../../types/supabase";
 
 interface RecTextProps {
-  rec: definitions['recommendations']
+  rec: definitions["recommendations"];
 }
 
 export const RecText = ({ rec }: RecTextProps) => {
+  const { colorMode } = useColorMode();
+  const isLight = colorMode === "light";
   return (
-    <Flex flexDir={'column'} px='1.5rem' pt='1rem' pb='2rem'>
-      <Text textStyle='subhead-2' color='primary.500'>
-        {(rec.cost ?? ' ').toUpperCase()}
+    <Flex
+      opacity={0.4}
+      backgroundColor={isLight ? "gray.100" : "white"}
+      alignSelf="flex-end"
+      flexDir={"column"}
+      px="1.5rem"
+      pt="1rem"
+      pb="2rem"
+    >
+      <Text textStyle="subhead-2" color="primary.500">
+        {(rec.cost ?? " ").toUpperCase()}
       </Text>
-      <Text textStyle='h2' color='neutral.800'>
+      <Text textStyle="h2" color="neutral.800">
         {rec.title}
       </Text>
-      <Text textStyle='body-1' color='neutral.800'>
+      <Text textStyle="body-1" color="neutral.800">
         {rec.description}
       </Text>
     </Flex>
-  )
-}
+  );
+};
