8d5535fdfb1155d59da1c88384da77c12568b931
Thor 雷神 Schaeff <5748289+thorwebdev@users.noreply.github.com>
2022-02-11 15:13:33 +0800

    feat: add README and og meta (#21)

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -2,11 +2,33 @@ import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 import { theme } from "../components/theme";
 
+const meta = {
+  title: 'unboringSG',
+  description: 'A website and browser extensions to discover things to eat, do, and learn.',
+  cardImage: '/unboringsg_og.png',
+};
+
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>{meta.title}</title>
+          <meta name="robots" content="follow, index" />
+          <link href="/favicon.ico" rel="shortcut icon" />
+          <meta content={meta.description} name="description" />
+          <meta property="og:url" content="https://unboring-sg.vercel.app/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content={meta.title} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:image" content={meta.cardImage} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@thorwebdev" />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.cardImage} />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
